3282b4ce2057e4d08b4111eeadcc5582dc31d850
Vadim Romendkii <vadim.romenskiy@bk.ru>
2022-09-03 16:50:53 +0300

    Проектная работа 4.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
 const popupEdit = document.querySelector('.profile__edit-button');
-const popupName = document.querySelector('.popup__input_name');
-const popupProfes = document.querySelector('.popup__input_profession');
+const popupName = document.querySelector('.popup__input-name');
+const popupProfes = document.querySelector('.popup__input-profession');
 const popupSubmit = document.querySelector('.popup__submit-button');
 const popupTitle = document.querySelector('.profile__name');
 const popupSubtitle = document.querySelector('.profile__text');
 
 popupEdit.addEventListener('click', () => {
-    popup.classList.add('popup_open');
+    popup.classList.add('popup_opened');
     popupName.value = popupTitle.textContent;
     popupProfes.value = popupSubtitle.textContent;
 })
 
 popupClose.addEventListener('click', () => {
-    popup.classList.remove('popup_open');
+    popup.classList.remove('popup_opened');
 })
 
 
@@ -24,7 +24,7 @@ function formSubmitHandler (evt) {
 evt.preventDefault();  
 popupTitle.textContent = popupName.value; 
 popupSubtitle.textContent = popupProfes.value; 
-popup.classList.remove('popup_open');
+popup.classList.remove('popup_opened');
 };
 
 popupForm.addEventListener('submit', formSubmitHandler); 
